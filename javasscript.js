d6f443415e97619cffb1a3ae463df47ce064988e
shiory602 <suzushioufo@gmail.com>
2023-03-24 19:08:13 +0400

    first commit

---
+++ javasscript.js
@@ -0,0 +1,21 @@
+// let products = ['apple', 'milk', 'bread', 'bag']
+
+// for (let i = 0; i < products.length; i++) {
+//   let word;
+//   let p = document.getElementById('title')
+//   // p.insertAdjacentHTML("beforeend", ' Hide');
+//   if(i = products.length-1) {
+//     word += ' ' + products[i] + '.';
+//     p.insertAdjacentHTML("beforeend", word);
+//   }
+//   word += ' ' + products[i] + ',';
+//   console.log(products[i]);
+//   p.insertAdjacentHTML("beforeend", word);
+// }
+
+
+// let products = ['apple', 'milk', 'bread', 'bag'];
+// console.log(products.join(', ') + '.');
+// let word = products.join(', ') + '.'
+// let p = document.getElementById('title')
+// p.insertAdjacentHTML("beforeend", word);
