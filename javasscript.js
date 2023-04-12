776def07f107b138d6989bf0cacbaf7a4cfcff89
shiory602 <suzushioufo@gmail.com>
2023-04-12 23:01:30 +0400

    first commit

--- javasscript.js
+++ javasscript.js
@@ -1,21 +1,20 @@
-// let products = ['apple', 'milk', 'bread', 'bag']
+$(function () {
 
-// for (let i = 0; i < products.length; i++) {
-//   let word;
-//   let p = document.getElementById('title')
-//   // p.insertAdjacentHTML("beforeend", ' Hide');
-//   if(i = products.length-1) {
-//     word += ' ' + products[i] + '.';
-//     p.insertAdjacentHTML("beforeend", word);
-//   }
-//   word += ' ' + products[i] + ',';
-//   console.log(products[i]);
-//   p.insertAdjacentHTML("beforeend", word);
-// }
+  //ページ内スクロール
+  var navHeight = $(".gnav").outerHeight();
 
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
 
-// let products = ['apple', 'milk', 'bread', 'bag'];
-// console.log(products.join(', ') + '.');
-// let word = products.join(', ') + '.'
-// let p = document.getElementById('title')
-// p.insertAdjacentHTML("beforeend", word);
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+});
